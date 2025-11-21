b5b235a6d23d68af2b894f1a6838c82ea2fd4ad0
qiliu0701 <qiliu0701@gmail.com>
2025-11-21 15:29:33 +0800

    .

--- Python/LSTM_RES_CNN/Architecture and parameters.py
+++ Python/LSTM_RES_CNN/Architecture and parameters.py
@@ -1,14 +1,6 @@
 import tensorflow as tf
 import numpy as np
-import data_prepare as dp
-import time
-input_data,output_data=dp.eva_or_train('train')
-input_data_lstm,output_data_lstm = [],[]
-for i in range(0,len(input_data) - 32,16):
-    input_data_lstm.append(input_data[i : i + 32])
-    output_data_lstm.append(output_data[i + 32])
-input_data_lstm=np.array(input_data_lstm)
-output_data_lstm=np.array(output_data_lstm)
+
 batch_size=64
 def create_model():
     inputs = tf.keras.Input(shape=(32, 6))
@@ -57,20 +49,3 @@ def train_step(inputs, targets):
     return loss_value
 
 epochs = 20000 +1
-for epoch in range(epochs):
-    start_clock = time.time()
-    for batch in range(0,len(input_data_lstm)-batch_size+1,batch_size):
-        start = batch
-        end = batch +  batch_size
-        inputs =input_data_lstm[start:end]
-        print(inputs.shape)
-        targets = output_data_lstm[start:end]
-        if batch==0:
-            loss = train_step(inputs, targets)
-        else:
-            loss = loss+train_step(inputs, targets)
-    if (epoch + 1) % 200 == 0:
-        print('epoch {}, loss:{}, time of each epoch:{:.2f} secs'.format(epoch + 1, loss, time.time() - start_clock))
-    if (epoch + 1) % 2000 == 0:
-        path = './ckpt_20251011_1/' + str(epoch + 1)
-        model.save(path)
